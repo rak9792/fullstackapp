8fffb698d5f468a14cfaa8eb8f2999249a4664f3
rakshit <rv09071992@gmail.com>
2018-05-28 17:09:33 +0530

    Latest changes

---
+++ src/main/java/com/fullstackapp/FullstackappApplication.java
@@ -0,0 +1,12 @@
+package com.fullstackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackappApplication.class, args);
+	}
+}
