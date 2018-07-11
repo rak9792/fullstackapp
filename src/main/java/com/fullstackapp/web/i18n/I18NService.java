3bedb16991148167bdd024a3e4cac0be4812df62
rakshit <rv09071992@gmail.com>
2018-07-11 16:34:28 +0530

    Added logging with logback and slf4j

--- src/main/java/com/fullstackapp/web/i18n/I18NService.java
+++ src/main/java/com/fullstackapp/web/i18n/I18NService.java
@@ -9,12 +9,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class I18NService {
-
+	
+		private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(I18NService.class);
+	
 		@Autowired
 		private MessageSource messageSource;
 		
 		public String getMessage(String messageId)
 		{
+			LOG.info("Returning i18n text for messageId {}",messageId);
 			Locale locale=LocaleContextHolder.getLocale();
 			return getMessage(messageId,locale);
 		}
